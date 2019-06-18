30fcb2f0624ab1dc5fe5b95b936d7872a91f707a
Alexandr <tolstov.alexandr.85@gmail.com>
2019-06-18 14:42:03 +0600

    Add project files.

---
+++ ComparableCar/Car.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ComparableCar
+{
+    class Car:IComparable
+    {
+        public const int MaxSpeed = 100;
+
+        public int CurrentSpeed { get; set; } = 0;
+        public string PetName { get; set; } = "";
+        private bool carIsDead;
+        private Radio theMusicBox = new Radio();
+
+        public int CarID { get; set; }
+        public Car() { }
+        public Car(string name, int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+        public Car(string name, int currSp, int id)
+        {
+            CurrentSpeed = currSp;
+            PetName = name;
+            CarID = id;
+        }
+        public void CrankTunes(bool state)
+        {
+            theMusicBox.TurnOn(state);
+        }
+        public void Accelerate(int delta)
+        {
+            if(carIsDead)
+                Console.WriteLine("{0} is out of order...", PetName);
+            else
+            {
+                CurrentSpeed += delta;
+                if(CurrentSpeed > MaxSpeed)
+                {
+                    CurrentSpeed = 0;
+                    carIsDead = true;
+
+                    Exception ex = new Exception($"{PetName} has overheated!");
+                    ex.HelpLink = "http://www.CarsRUs.com";
+                    ex.Data.Add("TimeStamp", $"The car exploded at {DateTime.Now}");
+                    ex.Data.Add("Cause", "You have a lead foot.");
+                    throw ex;
+                }
+                else
+                    Console.WriteLine("=> CurrentSpeed = {0}", CurrentSpeed);
+            }
+        }
+
+        public int CompareTo(object obj)
+        {
+            Car temp = obj as Car;
+            if (temp != null)
+            {
+                return this.CarID.CompareTo(temp.CarID);
+            }
+            else
+                throw new ArgumentException("Parameter is not a Car!");
+        }
+        public static IComparer SortByPetName
+        {
+            get
+            {
+                return (IComparer)new PetNameComparer();
+            }
+        }
+    }
+}
---
+++ ComparableCar/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ComparableCar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car[] myAutos = new Car[5];
+            myAutos[0] = new Car("Rusty", 80, 1);
+            myAutos[1] = new Car("Mary", 80, 234);
+            myAutos[2] = new Car("Viper", 80, 34);
+            myAutos[3] = new Car("Mel", 80, 4);
+            myAutos[4] = new Car("Chucky", 80, 5);
+
+            Console.WriteLine("Here is the unordered set of cars:");
+            foreach (var c in myAutos)
+            {
+                Console.WriteLine("{0} {1}", c.CarID, c.PetName);
+            }
+            Array.Sort(myAutos);
+            Console.WriteLine();
+
+            Console.WriteLine("Here is the ordered set of cars:");
+            foreach(Car c in myAutos)
+            {
+                Console.WriteLine("{0} {1}", c.CarID, c.PetName);
+            }
+
+            Array.Sort(myAutos, Car.SortByPetName);
+            Console.WriteLine("Ordering by pet name:");
+            foreach (Car c in myAutos)
+            {
+                Console.WriteLine("{0} {1}", c.CarID, c.PetName);
+            }
+            Console.ReadLine();
+        }
+    }
+}
